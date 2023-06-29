4ce458eef8299b20dd6671aacf3ad1637071e0f1
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-06-29 21:56:30 +0900

    出力時のフォーマットを実装

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,25 @@
+const prettier = require('prettier')
+
+let prettierOptions
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addTransform('formatHTML', async (content, outputPath) => {
+    if (!outputPath?.endsWith('.html')) {
+      return content
+    }
+
+    if (!prettierOptions) {
+      prettierOptions = await prettier.resolveConfig('test.html', {
+        editorconfig: true,
+      })
+    }
+
+    return prettier.format(content, {
+      ...prettierOptions,
+      parser: 'html',
+    })
+  })
+
   return {
     dir: {
       includes: '../includes',
